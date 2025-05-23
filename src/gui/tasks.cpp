200a9a1fe099f4187a8acd3c444533bbd79aa256
f0e <7321764+f0e@users.noreply.github.com>
2025-05-24 02:19:16 +1000

    fix: linux build

--- src/gui/tasks.cpp
+++ src/gui/tasks.cpp
@@ -86,7 +86,7 @@ void tasks::run(const std::vector<std::string>& arguments) {
 				"There's a newer version ({}) available! Click to go to the download page.", update_res.latest_tag
 			),
 			ui::NotificationType::INFO,
-			[&] {
+			[&](const std::string& id) {
 				SDL_OpenURL(update_res.latest_tag_url.c_str());
 			},
 			update_notification_duration
