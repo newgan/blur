1f7f95a866e28a1b1c95be98898a7b7d52156c90
f0e <7321764+f0e@users.noreply.github.com>
2025-08-14 22:46:56 +1000

    chore: bump version

--- src/common/blur.h
+++ src/common/blur.h
@@ -4,7 +4,7 @@
 #include "config_blur.h"
 
 const std::string APPLICATION_NAME = "blur";
-const std::string BLUR_VERSION = "2.4";
+const std::string BLUR_VERSION = "2.41";
 
 class Blur { // todo: switch all the classes which could be namespaces into namespaces
 public:
