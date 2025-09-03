c88ad9611a7258c56d31729eb13caf7610478b4f
newgan <laskocsgo@gmail.com>
2025-09-02 22:00:10 -0400

    chore: note to self

--- src/gui/ui/elements/videos.cpp
+++ src/gui/ui/elements/videos.cpp
@@ -151,6 +151,7 @@ namespace {
 			auto it = waveforms.find(key);
 
 			if (it == waveforms.end()) {
+				// todo: don't hardcode this, use calculated track rect
 				auto waveform = u::get_video_waveform(key, MIN_TRACK_WIDTH);
 
 				auto insert_result = waveforms.insert({ key, waveform });
